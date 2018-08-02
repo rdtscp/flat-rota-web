4b8089a4a9094af6c389c51819efd4eeb2c9d523
Alexander Wilson <acwilson96@gmail.com>
2018-08-02 17:57:02 +0100

    Default Views

---
+++ src/Views/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render = () => {
+    return (
+      <div className="landing-container">
+        <div style={{width: 300}}>
+          Login Page
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
