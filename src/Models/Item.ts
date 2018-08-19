5ba227f09f430a464a0935eb5c1d19912b561b1e
Alexander Wilson <acwilson96@gmail.com>
2018-08-19 12:09:59 +0100

    Create and Update API for Items

--- src/Models/Item.ts
+++ src/Models/Item.ts
@@ -1,17 +1,17 @@
 /* React/Redux/Other */
-// import axios, { AxiosResponse }                       from 'axios';
-// import axiosRetry                                     from 'axios-retry';
+import axios, { AxiosResponse }                       from 'axios';
+import axiosRetry                                     from 'axios-retry';
 
 /* This Project */
 import * as Models                                    from 'src/Models';
-// import    store                                       from 'src/Redux/Store';
-// import network                                        from 'src/Resources/networkHelper';
+import    store                                       from 'src/Redux/Store';
+import network                                        from 'src/Resources/networkHelper';
 
 interface IItemType {
   readonly id:          string;
   readonly flat:        Models.Flat;
   readonly name:        string;
-  readonly price:       string;
+  readonly description: string;
   readonly rota:        Models.User[];
 }
 
@@ -37,12 +37,33 @@ export const ItemAPI = {
     return '@TODO';
   },
 
-  create(username: string, password: string) {
-    // const authToken: string = store.getState().authState.authToken;
-    return '@TODO';
+  create(flatID: string, name: string, description: string) {
+    const authToken: string = store.getState().authState.authToken;
+    return new Promise((resolve, reject) => {
+      axiosRetry(axios, { retries: 10 });
+      network.getCSRF((csrf: string) => {
+        axiosRetry(axios, { retries: 0 });
+        axios.post(process.env.REACT_APP_API_URL + '/item/create', {
+          _csrf: csrf,
+          authToken,
+          description,
+          flatID,
+          name,
+        },{
+          withCredentials: true,
+        })
+        .then((response: AxiosResponse) => {
+          const data: ItemResponseData = response.data;
+          return resolve(data);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+      });
+    });
   },
 
-  destroy(deviceID: string, deviceAuthToken: string) {
+  destroy() {
     // const authToken: string = store.getState().authState.authToken;
     return '@TODO';
     // return new Promise((resolve, reject) => {
@@ -68,9 +89,29 @@ export const ItemAPI = {
     // });
   },
 
-  update() {
-    // const authToken: string = store.getState().authState.authToken;
-    return '@TODO';
+  update(itemID: string, flatID: string) {
+    const authToken: string = store.getState().authState.authToken;
+    return new Promise((resolve, reject) => {
+      axiosRetry(axios, { retries: 10 });
+      network.getCSRF((csrf: string) => {
+        axiosRetry(axios, { retries: 0 });
+        axios.post(process.env.REACT_APP_API_URL + '/item/update', {
+          _csrf: csrf,
+          authToken,
+          flatID,
+          itemID,
+        },{
+          withCredentials: true,
+        })
+        .then((response: AxiosResponse) => {
+          const data: ItemResponseData = response.data;
+          return resolve(data);
+        })
+        .catch((error) => {
+          return reject(error);
+        });
+      });
+    });
   }
 
 }
