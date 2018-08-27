249820c02cac2af29b60ae274a8dc966907a0571
Alexander Wilson <acwilson96@gmail.com>
2018-08-27 10:33:15 +0100

    Export Components in a single file

---
+++ src/Components/index.ts
@@ -0,0 +1,26 @@
+import AppNavigator           from './AppNavigator';
+import ConfirmDelete          from './ConfirmDelete';
+import Device                 from './Device';
+import DeviceList             from './DeviceList';
+import DrawerHeader           from './DrawerHeader';
+import Flat                   from './Flat';
+import FlatForm               from './FlatForm';
+import Item                   from './Item';
+import LoginForm              from './LoginForm';
+import SettingsMenu           from './SettingsMenu';
+import TodoList               from './TodoList';
+
+
+export {
+  AppNavigator,
+  ConfirmDelete,
+  Device,
+  DeviceList,
+  DrawerHeader,
+  Flat,
+  FlatForm,
+  Item,
+  LoginForm,
+  SettingsMenu,
+  TodoList,
+};
